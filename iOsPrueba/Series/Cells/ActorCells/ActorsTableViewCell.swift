7d46cbc8b1cd76456a46dd3689cec159ad27f421
JoakoDR <joaco_diaz_ramirez@hotmail.com>
2018-10-03 14:48:01 +0200

    ¡Tabla con los actores!

---
+++ iOsPrueba/Series/Cells/ActorCells/ActorsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActorsTableViewCell.swift
+//  iOsPrueba
+//
+//  Created by JOAQUIN DIAZ RAMIREZ on 3/10/18.
+//  Copyright © 2018 JOAQUIN DIAZ RAMIREZ. All rights reserved.
+//
+
+import UIKit
+
+class ActorsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

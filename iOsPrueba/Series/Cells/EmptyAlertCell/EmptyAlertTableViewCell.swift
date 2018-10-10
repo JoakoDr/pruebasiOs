f359bdb282872fd3244ce1be084748b897d275b8
JoakoDR <joaco_diaz_ramirez@hotmail.com>
2018-10-10 14:38:09 +0200

    Empty Alert Done

---
+++ iOsPrueba/Series/Cells/EmptyAlertCell/EmptyAlertTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmptyAlertTableViewCell.swift
+//  iOsPrueba
+//
+//  Created by JOAQUIN DIAZ RAMIREZ on 10/10/18.
+//  Copyright © 2018 JOAQUIN DIAZ RAMIREZ. All rights reserved.
+//
+
+import UIKit
+
+class EmptyAlertTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
